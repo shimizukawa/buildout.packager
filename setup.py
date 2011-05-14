fdcbc531feb11a497fb7f3ec8c8ed713218d1693
shimizukawa <devnull@localhost>
2011-05-14 18:07:00 +0900

    fixed: collect indent width 3 -> 4.

--- setup.py
+++ setup.py
@@ -9,37 +9,37 @@ long_description = \
   open(os.path.join("TODO.txt")).read()
 
 classifiers = [
-   "Development Status :: 4 - Beta",
-   "Environment :: Win32 (MS Windows)",
-   "Intended Audience :: Developers",
-   "License :: OSI Approved :: Apache Software License",
-#   "Operating System :: Microsoft :: Windows :: Windows NT/2000",
-   "Programming Language :: Python",
-   "Topic :: Software Development :: Build Tools",
-   "Topic :: Software Development :: Libraries :: Python Modules",
+    "Development Status :: 4 - Beta",
+    "Environment :: Win32 (MS Windows)",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+#    "Operating System :: Microsoft :: Windows :: Windows NT/2000",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Build Tools",
+    "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
 setup(
-    name='buildout.packager',
-    version=version,
-    description='buildout.packager generate installer from setup.py and buildout.cfg files.',
-    long_description=long_description,
-    classifiers=classifiers,
-    keywords=['zc.buildout','installer'],
-    author='Takayuki SHIMIZUKAWA',
-    author_email='shimizukawa at gmail.com',
-    url='http://bitbucket.org/shimizukawa/buildout.packager/',
-    license='Apache License 2.0',
-    package_dir={'': 'src'},
-    install_requires=[
-       'setuptools',
-       'py2exe',
-        # -*- Extra requirements: -*-
-    ],
-    entry_points="""
-       [distutils.commands]
-       bdist_buildout = buildout.packager.command.bdist_buildout:bdist_buildout
-       bdist_buildout_prepare = buildout.packager.command.bdist_buildout_prepare:bdist_buildout_prepare
-    """,
+     name='buildout.packager',
+     version=version,
+     description='buildout.packager generate installer from setup.py and buildout.cfg files.',
+     long_description=long_description,
+     classifiers=classifiers,
+     keywords=['zc.buildout','installer'],
+     author='Takayuki SHIMIZUKAWA',
+     author_email='shimizukawa at gmail.com',
+     url='http://bitbucket.org/shimizukawa/buildout.packager/',
+     license='Apache License 2.0',
+     package_dir={'': 'src'},
+     install_requires=[
+         'setuptools',
+         'py2exe',
+          # -*- Extra requirements: -*-
+     ],
+     entry_points="""
+         [distutils.commands]
+         bdist_buildout = buildout.packager.command.bdist_buildout:bdist_buildout
+         bdist_buildout_prepare = buildout.packager.command.bdist_buildout_prepare:bdist_buildout_prepare
+     """,
 )
 
