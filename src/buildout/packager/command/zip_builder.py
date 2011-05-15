5bff2aa027cf88c4663a52b950a7876888c7f3f7
shimizukawa <devnull@localhost>
2011-05-16 02:07:22 +0900

    refactoring

--- src/buildout/packager/command/zip_builder.py
+++ src/buildout/packager/command/zip_builder.py
@@ -9,12 +9,12 @@ def builder(name, installer_name, install_dir, src_dir, dist_dir,
            version='0.0.1', author_name=None, author_url=None,
            postfix_name=None, verbose=1):
 
-    data_files = []
-    for dirpath, dirnames, filenames in os.walk(src_dir):
-        data_files.extend(os.path.join(dirpath, x) for x in filenames)
-
     zipfile_name = to_filename(installer_name, version, postfix_name)
     zipfile_name += '.zip'
     log.info("Creating zip file: %s", zipfile_name)
+
+    data_files = []
+    for dirpath, dirnames, filenames in os.walk(src_dir):
+        data_files.extend(os.path.join(dirpath, x) for x in filenames)
 
     raise NotImplementedError('zip_builder.builder')
