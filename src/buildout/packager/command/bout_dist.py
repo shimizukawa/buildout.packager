948b37a02af609bd8df418ec07e50457b83aef30
shimizukawa <devnull@localhost>
2011-05-17 11:13:53 +0900

    added: zip archive impl.

--- src/buildout/packager/command/bout_dist.py
+++ src/buildout/packager/command/bout_dist.py
@@ -2,7 +2,7 @@
 from distutils.core import Command
 import os
 import sys
-from utils import resolve_interpreter, get_postfix_name
+from utils import resolve_interpreter, get_postfix_name, to_filename
 
 # from unix_builder import builder
 
@@ -76,6 +76,7 @@ class bout_zip(bout_dist):
     description = "create a buildout zip installer."
 
     def run (self):
+        cwd = os.getcwd()
         cmd_name = 'bout_src'
         sub_cmd = self.reinitialize_command(cmd_name)
         for option in ('src_dir', 'build_base', 'dist_dir', 'include_python', 'python'):
@@ -83,16 +84,12 @@ class bout_zip(bout_dist):
         self.run_command(cmd_name) #TODO: skip if prepared
 
         postfix_name = get_postfix_name(self.python)
-        build_dir = os.path.join(self.build_base, 'buildout-' + postfix_name)
+        build_dir = os.path.join(
+                cwd, self.build_base, 'buildout-' + postfix_name)
         meta = self.distribution.metadata
 
-        #### ä¬ã´ï ÇÃmake_packageÇåƒÇ—èoÇ∑
-        import zip_builder
-        zip_builder.builder(meta.name, meta.name, meta.name,
-                            build_dir,
-                            self.dist_dir,
-                            meta.version,
-                            meta.author,
-                            meta.url,
-                            postfix_name,
-                            self.verbose)
+        zipfile_name = to_filename(meta.name, meta.version, postfix_name)
+        filename = self.make_archive(
+                os.path.join(self.dist_dir, zipfile_name), 'zip',
+                root_dir=build_dir, base_dir=None)
+
