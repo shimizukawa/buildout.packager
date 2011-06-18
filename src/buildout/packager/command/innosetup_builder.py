ba993a8d7ae2003f06427072e40d7dfa4b13a366
shimizukawa <devnull@localhost>
2011-06-19 00:25:32 +0900

    refactoring: remove 'print >> file, ...' statement

--- src/buildout/packager/command/innosetup_builder.py
+++ src/buildout/packager/command/innosetup_builder.py
@@ -7,7 +7,51 @@ from distutils import log
 from utils import get_postfix_name, to_filename
 
 
-inno_setup_code_section = """
+MAIN_SCRIPT = r"""
+; WARNING: This script has been created by py2exe. Changes to this script
+; will be overwritten the next time py2exe is run!
+[Setup]
+AppName=%(self_name)s
+AppVerName=%(self_name)s %(self_version)s
+DefaultDirName={pf}\%(self_install_dir)s
+DefaultGroupName=%(self_name)s
+AllowUNCPath=no
+%(app_publisher)s
+%(app_publisher_url)s
+%(app_support_url)s
+OutputBaseFilename=%(self_installer_name)s
+OutputDir=%(self_dist_dir)s
+PrivilegesRequired=admin
+
+[Languages]
+Name: jp; MessagesFile: "compiler:Languages\Japanese.isl"
+
+[Files]
+%(data_files)s
+
+%(sys_files)s
+
+[Icons]
+%(icons)s
+Name: "{group}\Uninstall %(self_name)s"; Filename: "{uninstallexe}"
+
+[Registry]
+Root: HKCU; Subkey: "Software\%(self_author_name)s"; Flags: uninsdeletekeyifempty
+Root: HKCU; Subkey: "Software\%(self_author_name)s\%(self_name)s"; Flags: uninsdeletekeyifempty
+Root: HKCU; Subkey: "Software\%(self_author_name)s\%(self_name)s\%(self_version)s"; Flags: uninsdeletekey
+Root: HKLM; Subkey: "Software\%(self_author_name)s"; Flags: uninsdeletekeyifempty
+Root: HKLM; Subkey: "Software\%(self_author_name)s\%(self_name)s"; Flags: uninsdeletekeyifempty
+Root: HKLM; Subkey: "Software\%(self_author_name)s\%(self_name)s\%(self_version)s"; Flags: uninsdeletekey
+Root: HKLM; Subkey: "Software\%(self_author_name)s\%(self_name)s\%(self_version)s"; ValueType: string; ValueName: "InstallPath"; ValueData: "{app}"
+Root: HKLM; Subkey: "Software\%(self_author_name)s\%(self_name)s\%(self_version)s"; ValueType: string; ValueName: "Revision"; ValueData: "%(self_version)s"
+
+[UninstallDelete]
+Type: filesandordirs; Name: "{app}\parts";
+Type: filesandordirs; Name: "{app}\bin";
+Type: filesandordirs; Name: "{app}\develop-eggs";
+Type: filesandordirs; Name: "{app}\.installed.cfg";
+
+
 [Code]
 var
   FinishedInstall: Boolean;
@@ -73,68 +117,42 @@ class InnoScript(object):
         return pathname[len(self.src_dir):]
 
     def create(self):
-        ofi = open(self.iss_path, "w")
-        name = self.name
-        print >> ofi, "; WARNING: This script has been created by py2exe. Changes to this script"
-        print >> ofi, "; will be overwritten the next time py2exe is run!"
-        print >> ofi, r"[Setup]"
-        print >> ofi, r"AppName=%s" % name
-        print >> ofi, r"AppVerName=%s" % name + ' ' + self.version
-        print >> ofi, r"DefaultDirName={pf}\%s" % (self.install_dir)
-        print >> ofi, r"DefaultGroupName=%s" % name
-        print >> ofi, r"AllowUNCPath=no"
+        datastore = dict(('self_' + k, getattr(self, k)) for k in dir(self))
+        datastore['distribution_full_path'] = os.path.join(
+                self.dist_dir, self.installer_name)
+        datastore['app_publisher'] = ''
+        datastore['app_publisher_url'] = ''
+        datastore['app_support_url'] = ''
+
         if self.author_name:
-            print >> ofi, r"AppPublisher=%s" % self.author_name
+            datastore['app_publisher'] = "AppPublisher=%s" % self.author_name
         if self.author_url:
-            print >> ofi, r"AppPublisherURL=%s" % self.author_url
-            print >> ofi, r"AppSupportURL=%s" % self.author_url
-        print >> ofi, r"OutputBaseFilename=%s" % self.installer_name
-        print >> ofi, r"OutputDir=%s" % self.dist_dir
-        print >> ofi, r"PrivilegesRequired=admin"
+            datastore['app_publisher_url'] = "AppPublisherURL=%s" % self.author_url
+            datastore['app_support_url'] = "AppSupportURL=%s" % self.author_url
 
-        print >> ofi
-        print >> ofi, r"[Languages]"
-        print >> ofi, r'Name: jp; MessagesFile: "compiler:Languages\Japanese.isl"'
-        print >> ofi
-
-        print >> ofi, r"[Files]"
+        data_files = []
         for path in self.data_files:
-            print >> ofi, r'Source: "%s"; DestDir: "{app}\%s"; Flags: ignoreversion' % (path, os.path.dirname(path))
-        print >> ofi
+            dirname = os.path.dirname(path)
+            data_files.append(
+                r'Source: "%s"; DestDir: "{app}\%s"; Flags: ignoreversion' % (
+                path, dirname))
+        datastore['data_files'] = '\n'.join(data_files)
+
+        sys_files = []
         for path in self.sys_files:
-            print >> ofi, r'Source: "%s"; DestDir: "{sys}"; Flags: restartreplace uninsneveruninstall sharedfile comparetimestamp' % (path)
-        print >> ofi
+            sys_files.append(
+                r'Source: "%s"; DestDir: "{sys}"; Flags: restartreplace uninsneveruninstall sharedfile comparetimestamp' % path)
+        datastore['sys_files'] = '\n'.join(sys_files)
 
-        print >> ofi, r"[Icons]"
+        icons = []
         for name, path in self.exe_files:
-            print >> ofi, r'Name: "{group}\%s"; Filename: "{app}\%s"' % \
-                  (name, path)
-        print >> ofi, 'Name: "{group}\Uninstall %s"; Filename: "{uninstallexe}"' % self.name
-        print >> ofi
+            icons.append(
+                r'Name: "{group}\%s"; Filename: "{app}\%s"' %
+                (name, path))
+        datastore['icons'] = '\n'.join(icons)
 
-        d = dict(name=self.name, version=self.version, author_name=self.author_name)
-        print >> ofi, r'[Registry]'
-        print >> ofi, r'Root: HKCU; Subkey: "Software\%(author_name)s"; Flags: uninsdeletekeyifempty' % d
-        print >> ofi, r'Root: HKCU; Subkey: "Software\%(author_name)s\%(name)s"; Flags: uninsdeletekeyifempty' % d
-        print >> ofi, r'Root: HKCU; Subkey: "Software\%(author_name)s\%(name)s\%(version)s"; Flags: uninsdeletekey' % d
-        print >> ofi, r'Root: HKLM; Subkey: "Software\%(author_name)s"; Flags: uninsdeletekeyifempty' % d
-        print >> ofi, r'Root: HKLM; Subkey: "Software\%(author_name)s\%(name)s"; Flags: uninsdeletekeyifempty' % d
-        print >> ofi, r'Root: HKLM; Subkey: "Software\%(author_name)s\%(name)s\%(version)s"; Flags: uninsdeletekey' % d
-        print >> ofi, r'Root: HKLM; Subkey: "Software\%(author_name)s\%(name)s\%(version)s"; ValueType: string; ValueName: "InstallPath"; ValueData: "{app}"' % d
-        print >> ofi, r'Root: HKLM; Subkey: "Software\%(author_name)s\%(name)s\%(version)s"; ValueType: string; ValueName: "Revision"; ValueData: "%(version)s"' % d
-        print >> ofi
-
-        print >> ofi, r"[UninstallDelete]"
-        print >> ofi, r'Type: filesandordirs; Name: "{app}\parts";'
-        print >> ofi, r'Type: filesandordirs; Name: "{app}\bin";'
-        print >> ofi, r'Type: filesandordirs; Name: "{app}\develop-eggs";'
-        print >> ofi, r'Type: filesandordirs; Name: "{app}\.installed.cfg";'
-        print >> ofi
-
-        print >> ofi, inno_setup_code_section
-        print >> ofi
-
-        ofi.close()
+        with open(self.iss_path, "w") as f:
+            f.write(MAIN_SCRIPT % datastore)
 
     def compile(self):
         try:
