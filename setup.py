cc71be4f358182ae2c4f1eca6be6f6986d4218ee
shimizukawa <shimizukawa@gmail.com>
2012-10-29 21:24:57 +0900

    fix: can't install buildout.packager

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 version = '0.0.1'
@@ -29,8 +29,9 @@ setup(
      author='Takayuki SHIMIZUKAWA',
      author_email='shimizukawa at gmail.com',
      url='http://bitbucket.org/shimizukawa/buildout.packager/',
-     namespace_packages=['buildout'],
      license='Apache License 2.0',
+     namespace_packages=['buildout'],
+     packages=find_packages('src'),
      package_dir={'': 'src'},
      install_requires=[
          'setuptools',
