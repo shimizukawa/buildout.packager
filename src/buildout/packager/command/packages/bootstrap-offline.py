f6ef3a06104fe8a26253df005294d8a49af6a850
shimizukawa <shimizukawa@gmail.com>
2013-07-15 14:46:26 +0900

    fix pth reloading

--- src/buildout/packager/command/packages/bootstrap-offline.py
+++ src/buildout/packager/command/packages/bootstrap-offline.py
@@ -1,16 +1,17 @@
 import os
-import sys
 bjoin = lambda *a: os.path.join(os.path.dirname(__file__), *a)
-
-import site
-site.addsitedir(bjoin('..', 'eggs'))
-del site
-del sys.modules['site']
 
 from glob import glob
 import ez_setup
 ez_setup._install(sorted(glob(bjoin('../eggs/setuptools-*gz')))[-1])
 del ez_setup
 del glob
+
+import sys
+import site
+for d in [d for d in sys.path if 'site-packages' in d]:
+    site.addsitedir(d)
+del site
+del sys
 
 execfile(bjoin('bootstrap.py'))
